b1a9ddb00dcca3b8a6581c61623bc90c052fc216
mtreerungroj <m.treerungroj@gmail.com>
2018-03-26 23:44:19 +0700

    initial post activity page

--- src/components/protected/recordActivity/PostActivity.form.js
+++ src/components/protected/recordActivity/PostActivity.form.js
@@ -1,7 +1,45 @@
 import React, { Component } from 'react'
 
+import { grey500 } from 'material-ui/styles/colors'
+
 export default class PostActivityForm extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isLoading: true
+    }
+  }
+
+  async componentDidMount () {
+    this.setState({ isLoading: false })
+  }
+
   render () {
-    return <div>Post activity form</div>
+    return this.state.isLoading
+      ? <div>กำลังโหลดข้อมูล...</div>
+      : <div style={styles.container}>
+        <div><b>ข้อมูลผลการทำกิจกรรม</b></div>
+
+      </div>
+  }
+}
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginBottom: 30
+  },
+  rowDirection: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  underlineStyle: {
+    borderColor: grey500
+  },
+  checkboxContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginBottom: 10
   }
 }
