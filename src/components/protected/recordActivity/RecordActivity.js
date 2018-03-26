de1de4d185e5a33dfcce618c924a5c02c865610e
mtreerungroj <m.treerungroj@gmail.com>
2018-03-26 16:35:30 +0700

    add header and styles

--- src/components/protected/recordActivity/RecordActivity.js
+++ src/components/protected/recordActivity/RecordActivity.js
@@ -6,6 +6,8 @@ import ResultActivityForm from './Result.form'
 
 import { getUserStatus } from '../../../services/helpers'
 
+import { grey300 } from 'material-ui/styles/colors'
+
 export default class RecordActivity extends Component {
   constructor (props) {
     super(props)
@@ -26,11 +28,40 @@ export default class RecordActivity extends Component {
       : this.state.profile.role !== 'nurse' &&
           this.state.profile.role !== 'doctor'
           ? <div>Inaccessible</div>
-          : <div>
-            <InformationForm />
-            <PreActivityForm />
-            <PostActivityForm />
-            <ResultActivityForm />
+          : <div style={styles.container}>
+            <div style={styles.inner}>
+              <div style={styles.header}>
+                  บันทึกผลการทำกิจกรรมฟื้นฟูสมรรถภาพห้วใจของผู้ป่วยย้อนหลัง
+                </div>
+            </div>
+            <div style={styles.content}>
+              <InformationForm />
+              <PreActivityForm />
+              <PostActivityForm />
+              <ResultActivityForm />
+            </div>
           </div>
+  }
+}
+
+const styles = {
+  container: {
+    backgroundColor: grey300,
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  inner: {
+    flex: 1
+  },
+  header: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 20,
+    marginTop: 20,
+    marginBottom: 20
+  },
+  content: {
+    marginLeft: 30,
+    marginRight: 30
   }
 }
